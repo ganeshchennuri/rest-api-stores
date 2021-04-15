00510a23e42523fa4e37cad7c0e1e825d279b8eb
Ganesh Chennuri <ganeshchennuri5@gmail.com>
2021-04-16 00:36:19 +0530

    User & Store resource is implemented

--- resources/store.py
+++ resources/store.py
@@ -1,8 +1,33 @@
 from flask_restful import Resource
+from models.store import StoreModel
 
 class Store(Resource):
-    def get(self):
-        pass
+    def get(self,name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json()
+        return {"message": "Store Not Found"},404
+    
+    def post(self,name):
+        if StoreModel.find_by_name(name):
+            return {"message": "Store with {} name already exists".format(name)}
 
-    def post(self):
-        pass
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            {"messsage": "Error occured while creating store"},500
+                
+        return store.json(),201
+
+    def delete(self,name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+            return {"message": "Store Deleted"}
+        return {"message": "Store doesnot exist"}
+
+
+class StoreList(Resource):
+    def get(self):
+        return {"stores": [store.json() for store in StoreModel.query.all()]}
--- resources/user.py
+++ resources/user.py
@@ -1,3 +1,25 @@
-class Store(Resource):
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("username",
+        type=str,
+        required=True,
+        help="This Field is required"
+    )
+    parser.add_argument("password",
+        type=str,
+        required=True,
+        help="This Field is required"
+    )
+
     def post(self):
-        pass
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data["username"]):
+            return {"message": "User with username Already Exists"}, 400
+        
+        user = UserModel(**data)
+        user.save_to_db()
+        return {"message": "User Created Successfully"}, 201
