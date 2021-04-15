7570e282b2820a3bb9df13667341edde9d98d1fd
Ganesh Chennuri <ganeshchennuri5@gmail.com>
2021-04-16 00:29:29 +0530

    Item resource is implemented

--- resources/item.py
+++ resources/item.py
@@ -1,19 +1,60 @@
-from flask_restful import Resource
+from flask_restful import Resource,reqparse
+from models.item import ItemModel
 
 class Item(Resource):
-    def get(self):
-        pass
+    parser = reqparse.RequestParser() #Parsing the body using reqparse
+    parser.add_argument("price",
+        type=float,
+        required=True,
+        help="This Field cannot be empty"
+        )
+    parser.add_argument("store_id",
+        type=int,
+        required=True,
+        help="store id is required"
+        )
+
+    def get(self, name):
+        try:
+            item = ItemModel.find_by_name(name)
+            if item:
+                return item.json()
+            return {"message": "item not found"},404
+        except:
+            return {"message": "Error while fetching item details"}, 500
 
     def post(self):
-        pass
+        if ItemModel.find_by_name(name):
+            return {"message": "An item with {} name already exists".format(name)}
+
+        data = Item.parser.parse_args()
+        item = ItemModel(name,**data)
+        try:
+            item.save_to_db()
+        except:
+            return {"message": "Error while inserting item"}, 500
+        return item.json(),201
 
     def put(self):
-        pass
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            item = ItemModel(name,**data)
+        else:
+            item.price = data['price']
+            
+        item.save_to_db()
+        return item.json()
 
     def delete(self):
-        pass
+        item = ItemModel.find_by_name(name)
+        if item is None:
+            return {"message": "Item not found"},404
+        item.delete_from_db()
+        return {"message": "item deleted successfully"}
 
 
 class Itemlist(Resource):
     def get():
-        pass
+        return {"items": [item.json() for item in ItemModel.query.all()]}
